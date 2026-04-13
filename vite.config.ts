ea2b5ea7670f86a64339ce0f8aa69099a7ebefc0
Humayun-23 <humayunroshid2@gmail.com>
2026-04-14 00:53:06 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["ride-elegance.onrender.com"],
     hmr: {
       overlay: false,
     },
