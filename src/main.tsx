2ed678715f8bb13a5d739047379e7f0ba5d3cdf2
Humayun-23 <humayunroshid2@gmail.com>
2026-05-21 23:55:37 +0530

    Added google analytics tag

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,5 @@ import './index.css'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
 )
